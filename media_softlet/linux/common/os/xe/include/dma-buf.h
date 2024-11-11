665e36a516c784e930c6e2c0eb64749818f50276
Aman Karmani <aman@tmm1.net>
2024-11-11 12:21:27 -0800

    freebsd fix

--- media_softlet/linux/common/os/xe/include/dma-buf.h
+++ media_softlet/linux/common/os/xe/include/dma-buf.h
@@ -20,7 +20,23 @@
 #ifndef _DMA_BUF_UAPI_H_
 #define _DMA_BUF_UAPI_H_
 
+#ifdef __linux__
 #include <linux/types.h>
+#else /* One of the BSDs */
+#include <stdint.h>
+#include <sys/ioccom.h>
+#include <sys/types.h>
+typedef int8_t   __s8;
+typedef uint8_t  __u8;
+typedef int16_t  __s16;
+typedef uint16_t __u16;
+typedef int32_t  __s32;
+typedef uint32_t __u32;
+typedef int64_t  __s64;
+typedef uint64_t __u64;
+typedef size_t   __kernel_size_t;
+typedef unsigned long drm_handle_t;
+#endif
 
 /**
  * struct dma_buf_sync - Synchronize with CPU access.
